7836ad0fcc29e57c045fb9cf1fc3e09558ccfdd4
bader <bader5uk@hotmail.com>
2024-10-01 09:57:22 +0400

    Rearrange an array to move 4 next to a 3

---
+++ RearrangeArray.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class RearrangeArray {
+    /*
+    Input: An array of integers that contains a three and a four
+    Output: the array with 3s and 4s re-arranged to be next to each other
+    Process: - 2 for loops that check the element
+             - if it is equal to 3, dont move
+             - if equal to 4 then move it next to the 3
+     */
+    public static void main(String[]args){
+        int[] arrayOfIntegers = {1, 5, 6, 3, 20 , 5, 4, 3, 4};
+        int store = 0;
+
+        for(int i = 0; i<arrayOfIntegers.length; i++){
+            if(arrayOfIntegers[i]==3){
+                int j;
+                j=i+1;
+                while (arrayOfIntegers[j] != 4){
+                    j++;
+                }
+                store = arrayOfIntegers[i+1];
+                arrayOfIntegers[i+1]=4;
+                arrayOfIntegers[j] = store;
+            }
+        }       System.out.println(Arrays.toString(arrayOfIntegers));
+    }
+}
