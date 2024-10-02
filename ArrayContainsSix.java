e468ad2eb3dcf3e2d235e35aa95f8f178afa9de3
bader <bader5uk@hotmail.com>
2024-10-02 13:02:50 +0400

    Cleaned code and used List instead of primitive data types

--- ArrayContainsSix.java
+++ ArrayContainsSix.java
@@ -1,17 +1,23 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class ArrayContainsSix {
+    /*
+    Checks if the list contains a 6 at the start or end of and returns a Boolean if true.
+     */
 
 public static void main(String[] args) {
-        int[] arrayOfInt = {6, 25, 9, 500, 5};
-        boolean checker = false;
-        if ( arrayOfInt[0] == 6 && arrayOfInt[arrayOfInt.length - 1] == 6) {
+    List<Integer> arrayofInt = Arrays.asList(6, 25, 9, 500, 6);
+        Boolean checker = false;
+        if (arrayofInt.getFirst() == 6 && arrayofInt.get(arrayofInt.size() - 1) == 6) {
             checker = true;
             System.out.println(checker);
         }
-        else if ( arrayOfInt[arrayOfInt.length - 1] == 6){
+        else if (arrayofInt.get(arrayofInt.size() - 1) == 6){
             checker = true;
             System.out.println(checker);
         }
-        else if ( arrayOfInt[0] == 6){
+        else if (arrayofInt.getFirst() == 6){
             checker = true;
             System.out.println(checker);
         }
