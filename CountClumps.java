aed528fb13e0f43d5dd19054cd6f911b8a95f694
bader <bader5uk@hotmail.com>
2024-10-02 11:45:40 +0400

    changed array method to use Array List

--- CountClumps.java
+++ CountClumps.java
@@ -1,19 +1,24 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class CountClumps {
     public static void main(String[] args) {
-        int[] t = {1,1,2,2,2,3,4,4,4,5,6,7,7};
+        List<Integer> t = Arrays.asList(1,1,2,2,3,4,4,4,5,6,7,7);
+
         int repeat = 0;
         boolean[] clumps = new boolean[9];
-        for (int i = 0; i < t.length; i++) {
-            int element = t[i];
-            for (int j = i + 1; j < t.length; j++) {
-                if (element == t[j]) {
-                    int index = t[j];
+        for (int i = 0; i < t.size(); i++) {
+            int element = t.get(i);
+            for (int j = i + 1; j < t.size(); j++) {
+                if (element == t.get(j)) {
+                    int index = t.get(j);
                     clumps[index] = true;
-                } else if (element != t[j]) {
+                } else if (element != t.get(j)) {
                     i = j - 1;
                     break;
                 }
-                if(j == t.length -1){
+                if(j == t.size() -1){
                     i = j;
                 }
             }
