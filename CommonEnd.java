6f3218a8abc949dcd30f83e9fb1e1cbe6852770b
bader <bader5uk@hotmail.com>
2024-10-02 12:32:21 +0400

    Cleaned code and used List instead of primitive data types

--- CommonEnd.java
+++ CommonEnd.java
@@ -1,13 +1,16 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class CommonEnd {
 
     public static void main(String[] args) {
-        int[] firstArray = {5, 25, 9, 500, 2};
-        int[] secondArray = {5, 6};
-        if (firstArray[firstArray.length - 1] == secondArray[secondArray.length - 1]) {
+        List<Integer> firstArray = Arrays.asList(5, 25, 9, 500, 6);
+        List<Integer> secondArray = Arrays.asList(5, 6);
+        if (firstArray.getFirst() == secondArray.get(secondArray.size() - 1) || firstArray.get(firstArray.size() - 1) == secondArray.getFirst()) {
             System.out.println("The two Arrays share the last element");
-        } else if (firstArray[0] == secondArray[secondArray.length - 1] || firstArray[firstArray.length - 1] == secondArray[0]) {
+        } else if (firstArray.get(firstArray.size() - 1) == secondArray.get(secondArray.size() - 1)) {
             System.out.println("The two Arrays share the last and first element");
-        } else if (firstArray[0] == secondArray[0]) {
+        } else if (firstArray.getFirst() == secondArray.getFirst()) {
             System.out.println("The two Arrays share the same first element");
         } else {
             System.out.println("The Arrays dont share the first or last element");
