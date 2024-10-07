ecdbab2c1faf42ad5affcb0788bab5c9289bcc13
bader <bader5uk@hotmail.com>
2024-10-07 09:41:00 +0400

    Cleaned code and used List, changed while loop to if statement

--- RearrangeArray.java
+++ RearrangeArray.java
@@ -1,5 +1,6 @@
-import java.util.ArrayList;
+
 import java.util.Arrays;
+import java.util.List;
 
 public class RearrangeArray {
     /*
@@ -10,24 +11,15 @@ public class RearrangeArray {
              - if equal to 4 then move it next to the 3
      */
     public static void main(String[]args){
-         ArrayList<Integer> arrayOfIntegers = new ArrayList<>();
-         arrayOfIntegers.add(1);
-         arrayOfIntegers.add(3);
-         arrayOfIntegers.add(4);
-         arrayOfIntegers.add(3);
-         arrayOfIntegers.add(20);
-         arrayOfIntegers.add(5);
-         arrayOfIntegers.add(4);
-         arrayOfIntegers.add(3);
-         arrayOfIntegers.add(4);
-                // {1, 3, 4, 3, 20 , 5, 4, 3, 4};
+         List<Integer> arrayOfIntegers = Arrays.asList(1,3,4,3,20,5,4,3,4);
+
         int store = 0;
 
-        for(int i = 0; i<arrayOfIntegers.indexOf(i); i++){
+        for(int i = 0; i<arrayOfIntegers.size(); i++){
             if(arrayOfIntegers.get(i) ==3){
                 int j;
                 j=i+1;
-                while (arrayOfIntegers.get(j) != 4){
+                if (arrayOfIntegers.get(j) != 4){
                     j++;
                 }
                 store = arrayOfIntegers.get(i + 1);
